d0ce6eb0c7f2b9e751806ef88d09e56455712d39
VishalMORE-neosoft <more.vishal@neosoftmail.com>
2025-12-25 22:46:29 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import RegexBuilder from './widgets/regex-builder';
-import Dashboard from './components/pages/Dashboard';
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+import RegexBuilder from "./widgets/regex-builder";
+import Dashboard from "./components/pages/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Dashboard />} />
-        <Route path="/regex-builder" element={<RegexBuilder />} />
+        <Route path="/" element={<Outlet />}>
+          <Route index element={<Dashboard />} />
+          <Route path="regex-builder" element={<RegexBuilder />} />
+        </Route>
         <Route path="*" element={<div>Not Found</div>} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
